408fa5a3d7be3f90d6b3c8ef5694715f2a577ddf
Amanda Smith <asmith@clearleap.com>
2015-08-23 03:33:14 -0400

    proper parsing of params

--- scripts/parselogs.sh
+++ scripts/parselogs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 PARAM="foo bar bob"
-if [ -z $1 ]; then
-	PARAM=$1
+if [ ! -z "$1" ]; then
+	PARAM="$1"
 fi
 
-cat $1 | grep "love by baby"
+cat $PARAM | grep "love by baby"
 
 time echo "everything is awesome"
 echo "when you are part of a team"
